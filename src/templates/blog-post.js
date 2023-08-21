8c91ea197a764eaa28abefa222d11b1147137270
coconu6956 <wce0422@gmail.com>
2023-08-21 17:18:42 +0900

    blog nav 디자인 수정

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -41,14 +41,14 @@ const BlogPostTemplate = ({
             padding: 0,
           }}
         >
-          <li>
+          <li className="post-nav-link">
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
-          <li>
+          <li className="post-nav-link">
             {next && (
               <Link to={next.fields.slug} rel="next">
                 {next.frontmatter.title} →
